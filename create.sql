f7800ff18e808f2b58adc6c7d692b836f99e271a
Rheinmir <rheinmir@gmail.com>
2024-03-22 21:27:27 +0700

    CREATE

---
+++ create.sql
@@ -0,0 +1,15 @@
+/*create new database*/
+CREATE DATABASE rhein_php CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
+
+/*use this database*/
+use rhein_php;
+
+/*create new table*/
+CREATE TABLE student (
+    id int,
+    fullname VARCHAR(100),
+    age int,
+    address VARCHAR(200)
+)
+    
+
