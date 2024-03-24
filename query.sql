72ba41b2ed3e5a0260a08402707a7f47891609d6
Rheinmir <rheinmir@gmail.com>
2024-03-24 13:59:14 +0700

    select

--- query.sql
+++ query.sql
@@ -9,6 +9,9 @@ ORDER BY age ASC;
 /* select all data and limit result */
 SELECT * FROM college_student
 LIMIT 2;
-/* select all data with condition and limit result */
+/* select all data met condition and limit result */
 SELECT * FROM college_student
 WHERE AGE >=20 LIMIT 2;
+/* select all data  from specified index(from 0) and quantity*/
+SELECT * FROM college_student
+LIMIT 1,2;
